75c3953e7ca7c5e483495edce9186efeabf02f88
Heta Patel <230390116016@saffrony.ac.in>
2026-06-11 18:13:55 +0530

    Add Vercel URL to CORS

--- backend/main.py
+++ backend/main.py
@@ -49,6 +49,7 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://localhost:3001",
         "http://127.0.0.1:3001",
+         "https://smart-timetable-assistant-weld.vercel.app",
     ],          # In prod, replace with your Vercel URL
     allow_credentials=True,
     allow_methods=["*"],
